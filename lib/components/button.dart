25f535d4863a43d33291dca0dcdb16774873b87a
Don Olmstead <don.j.olmstead@gmail.com>
2013-05-27 13:08:07 -0700

    Adding initial button implementation

---
+++ lib/components/button.dart
@@ -0,0 +1,13 @@
+// Copyright (c) 2013, the Pixelate Project Authors.
+// Please see the AUTHORS file for details. All rights reserved.
+// Use of this source code is governed by a zlib license that can be found in
+// the LICENSE file.
+
+import 'dart:html';
+import 'dart:async';
+import 'package:web_ui/web_ui.dart';
+import 'button_base.dart';
+
+/// The base class for all button controls.
+class ButtonComponent extends ButtonBaseComponent {
+}
