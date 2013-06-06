42be44d7eecaf113f3339a8997559357364fbb47
Don Olmstead <don.j.olmstead@gmail.com>
2013-06-05 17:19:23 -0700

    Adding comments

--- lib/components/button.dart
+++ lib/components/button.dart
@@ -8,6 +8,40 @@ import 'dart:async';
 import 'package:web_ui/web_ui.dart';
 import 'package:pixelate/attribute_helpers.dart';
 
+/// A button control.
+///
+/// The element can be disabled in markup by using the disabled attribute,
+/// which takes a boolean string value. This prevents the onClick event from
+/// being fired. By default the control is enabled.
+///
+///     <!-- Enabled; the default -->
+///     <div is="x-expander" disabled="false"></div>
+///     <!-- Disabled -->
+///     <div is="x-expander" expanded="true"></div>
+///
+/// The [ExpanderComponent] defines styles for the button colors, and the
+/// transitions between them.
+///
+///     div[is=x-button] {
+///       /* The default button color */
+///       -webkit-var-button-background-color: ___;
+///       /* The button color used when the cursor is hovering over the area */
+///       -webkit-var-button-hover-background-color: ___;
+///       /* The button color used when the cursor is being depressed */
+///       -webkit-var-button-active-background-color: ___;
+///       /* The button color used when disabled */
+///       -webkit-var-button-disabled-background-color: ___;
+///       /* The transition function used when transitioning the button color */
+///       -webkit-var-button-color-transition-timing-function: ___;
+///       /* The time it takes to transition between colors within the button */
+///       -webkit-var-button-color-transition-duration: ___;
+///       /* The color of the button's border */
+///       -webkit-var-button-border-color: ___;
+///       /* The style of the button's border */
+///       -webkit-var-button-border-style: ___;
+///       /* The width of the button's border */
+///       -webkit-var-button-border-width: ___;
+///     }
 class ButtonComponent extends WebComponent {
   //---------------------------------------------------------------------
   // Class variables
@@ -63,8 +97,12 @@ class ButtonComponent extends WebComponent {
     _onClick = _onClickController.stream;
 
     // Connect to the host onClick event
-    host.onClick.listen(_onHostClick);////(e) { _onHostClick(e); });
+    host.onClick.listen(_onHostClick);
   }
+
+  //---------------------------------------------------------------------
+  // Private methods
+  //---------------------------------------------------------------------
 
   /**
    * Callback for when the host element is clicked.
@@ -74,8 +112,6 @@ class ButtonComponent extends WebComponent {
   void _onHostClick(MouseEvent event) {
     if ((!_disabled) && (!_onClickController.isPaused)) {
       _onClickController.add(event);
-    } else {
-      print('nope nope');
     }
   }
 }
