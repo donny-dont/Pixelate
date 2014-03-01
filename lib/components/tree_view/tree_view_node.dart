3e6ac6d006f05a0e7b87072721b8e0245f4097ac
Don <don.j.olmstead@gmail.com>
2014-03-01 12:58:56 -0800

    Changing around HTML to match the expander.html contents

--- lib/components/tree_view/tree_view_node.dart
+++ lib/components/tree_view/tree_view_node.dart
@@ -17,6 +17,7 @@ import 'dart:html';
 //---------------------------------------------------------------------
 
 import 'package:polymer/polymer.dart';
+import 'package:pixelate/customizable.dart';
 import 'package:pixelate/expandable.dart';
 import 'package:pixelate/selectable.dart';
 
@@ -29,37 +30,37 @@ const String _tagName = 'px-tree-view-node';
 
 
 @CustomTag(_tagName)
-class TreeViewNode extends PolymerElement with Expandable, Selectable {
+class TreeViewNode extends PolymerElement with Expandable, Customizable, Selectable {
   /// The name of the tag.
   static String get customTagName => _tagName;
-  
+
   static String elementIdHost = "node_host";
-  
+
   /// Css class to apply on an selected element. Required by the Selectable mixin
   String get cssClassItemSelected => "node_selected";
-  
+
   /// Css class to apply on an unselected element. Required by the Selectable mixin
   String get cssClassItemUnSelected => "node_unselected";
 
   /// The element affected by the selection state. Used by the Selectable mixin
   Element get selectionElement => elementNode;
-  
+
   /// The text displayed on the tree node
   @published String text = "Node";
-  
+
   /// The Id of the tree node. This id is used when raising events
   @published String id = "node";
-  
+
   /// The Url of the icon to be used for this node
   @published String icon = "";
-  
-  /// The host DOM element of the tree node 
+
+  /// The host DOM element of the tree node
   Element elementNode;
-  
+
   /// This div element displays the icon as a background image
   Element elementNodeIcon;
-  
-  
+
+
   /// Indicates the expanded/collapsed state of the tree node
   bool _expanded = false;
   bool get expanded => _expanded;
@@ -67,21 +68,20 @@ class TreeViewNode extends PolymerElement with Expandable, Selectable {
     var oldValue = _expanded;
     _expanded = value;
     expandedChanged(oldValue);
-  } 
-  
-  /// The node expander views
-  Element _expanderView;
-  Element _expanderContent;
+  }
 
-  /// The view of the content area.
-  Element get view => _expanderView;        //TODO: Refactor view to expanderView to give context
-  /// The content area to expand or contract.
-  Element get content => _expanderContent;  //TODO: Refactor content to expanderContent to give context
-  
-  
-  /// The node expander icon.  
+  /// The view for the expandable portion.
+  ///
+  /// Required for the [Expandable] mixin to function.
+  Element _view;
+  /// The content contained in the expandable portion.
+  ///
+  /// Required for the [Expandable] mixin to function.
+  Element _content;
+
+  /// The node expander icon.
   TreeViewNodeExpanderIcon expanderIcon;
-  
+
   /// Create an instance of the [TreeViewNode] class.
   ///
   /// This constructor should not be called directly. Instead use the
@@ -91,33 +91,47 @@ class TreeViewNode extends PolymerElement with Expandable, Selectable {
   TreeViewNode.created()
       : super.created()
   {
+    // Initialize the expandable mixin
+    var shadowRoot = getShadowRoot(customTagName);
+
+    _content = shadowRoot.querySelector('.expandable');
+    _view = shadowRoot.querySelector('.view');
+
+    initializeExpandable();
+
+    // Intitialize the customizable mixin
+    initializeCustomizable();
+    customizeProperty(#header, 'tree-view-header', 'default-tree-view-header');
   }
+
+  //---------------------------------------------------------------------
+  // Expandable properties
+  //---------------------------------------------------------------------
+
+  Element get content => _content;
+  Element get view => _view;
 
   void enteredView() {
     super.enteredView();
 
-    _expanderView = this.shadowRoot.querySelector("#children_host");
-    _expanderContent = this.shadowRoot.querySelector("#expander");
     elementNode = shadowRoot.querySelector("#$elementIdHost");
     elementNode.onDoubleClick.listen(onNodeDoubleClicked);
     elementNodeIcon = shadowRoot.querySelector("#node_icon");
     _updateIcon();
-    
+
     expanderIcon = new TreeViewNodeExpanderIcon(this);
-    initializeExpandable();
-    
   }
-  
+
   String toString() => text;
-  
+
   void onNodeClicked(Event e) {
     selected = true;
   }
-  
+
   void onNodeDoubleClicked(Event e) {
     toggleExpand();
   }
-  
+
   /// Toggles the expander state
   void toggleExpand() {
     this.toggle();  // TODO: Refactor Expandable mixing from toggle to toggleExpand to give context
@@ -129,27 +143,27 @@ class TreeViewNode extends PolymerElement with Expandable, Selectable {
   void onCollapse() {
     expanderIcon.update();
   }
-  
-  /// Expands all the child nodes 
+
+  /// Expands all the child nodes
   void expandAll() {
     expanded = true;
     nodes.forEach((node) {
       if (node is TreeViewNode) {
         node.expandAll();
-      } 
+      }
     });
   }
-  
+
   /// Collapses all the nodes in the tree view
   void collapseAll() {
     expanded = false;
     nodes.forEach((node) {
       if (node is TreeViewNode) {
         node.collapseAll();
-      } 
+      }
     });
   }
-  
+
   /// Updates the DOM to reflect the icon property
   void _updateIcon() {
     if (icon.length > 0) {
@@ -168,12 +182,12 @@ class TreeViewNodeExpanderIcon {
     elementIcon.onClick.listen(onClicked);
     update();
   }
-  
+
   /// Updates the state of the DOM elements based on the expanded / collapsed state
   void update() {
     elementIcon.classes.add(node.expanded ? cssNameIconExpanded : cssNameIconCollapsed);
     elementIcon.classes.remove(node.expanded ? cssNameIconCollapsed : cssNameIconExpanded);
-    
+
     // Hide the icon if this belongs to a leaf node
     bool visible = (node.nodes.length > 0);
     if (visible) {
@@ -182,7 +196,7 @@ class TreeViewNodeExpanderIcon {
       elementIcon.classes.add("hidden");
     }
   }
-  
+
   void onClicked(Event e) {
     node.toggleExpand();
   }
