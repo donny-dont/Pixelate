a33dfd15a8830622408531276d5bb308eb05dd32
Don Olmstead <don.j.olmstead@gmail.com>
2014-03-01 20:12:13 -0800

    Fixing package paths in grid

--- lib/components/grid/grid_panel.dart
+++ lib/components/grid/grid_panel.dart
@@ -17,8 +17,8 @@ import 'dart:html' as Html;
 //---------------------------------------------------------------------
 
 import 'package:polymer/polymer.dart';
-import 'package:pixelate/components/column_definitions.dart';
-import 'package:pixelate/components/row_definitions.dart';
+import 'package:pixelate/components/grid/column_definitions.dart';
+import 'package:pixelate/components/grid/row_definitions.dart';
 
 //---------------------------------------------------------------------
 // Library contents
