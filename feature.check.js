540ea18e245e537b61c835a436859d8045f4dcc4
Don <don.j.olmstead@gmail.com>
2014-03-23 22:42:45 -0700

    Updating the feature checks for IE

--- feature.check.js
+++ feature.check.js
@@ -1,13 +1,19 @@
 (function () {
-  var chromeVersion = parseInt(window.navigator.appVersion.match(/Chrome\/(\d+)\./)[1], 10);
+  var chromeMatch = window.navigator.appVersion.match(/Chrome\/(\d+)\./);
 
-  console.log('Chrome ' + chromeVersion);
+  if (chromeMatch) {
+    var chromeVersion = parseInt(chromeMatch[1], 10);
 
-  if (chromeVersion >= 35) {
-    window.alert('This site will not work properly until Chrome 35 is promoted to stable. Changes were made to Shadow DOM styling which are not currently present. Please use an earlier version.')
-  } else if (chromeVersion >= 32) {
-    if (typeof window.HTMLDivElement.prototype['createShadowRoot'] == 'undefined') {
-      window.prompt('This site uses experimental web platform features. To view paste the following into another tab. Enable the feature and restart Chrome', 'chrome://flags/#enable-experimental-web-platform-features');
+    console.log('Chrome ' + chromeVersion);
+
+    if (chromeVersion >= 35) {
+      window.alert('This site will not work properly until Chrome 35 is promoted to stable. Changes were made to Shadow DOM styling which are not currently present. Please use an earlier version.')
+    } else if (chromeVersion >= 32) {
+      if (typeof window.HTMLDivElement.prototype['createShadowRoot'] == 'undefined') {
+        window.prompt('This site uses experimental web platform features. To view paste the following into another tab. Enable the feature and restart Chrome', 'chrome://flags/#enable-experimental-web-platform-features');
+      }
+    } else {
+      window.alert('This site is only viewable in the latest stable version of Chrome');
     }
   } else {
     window.alert('This site is only viewable in the latest stable version of Chrome');
