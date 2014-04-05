1e14356ff9f2e33cef288d0a31a3e64345777037
Don Olmstead <don.j.olmstead@gmail.com>
2014-04-04 21:50:17 -0700

    Removing build.dart entry-points as they aren't used anymore. Moving back to path for the pubspec for pixelate_flat

--- build.dart
+++ build.dart
@@ -1,4 +1,4 @@
-// Copyright (c) 2013, the Pixelate Project Authors.
+// Copyright (c) 2013-2014, the Pixelate Project Authors.
 // Please see the AUTHORS file for details. All rights reserved.
 // Use of this source code is governed by a zlib license that can be found in
 // the LICENSE file.
@@ -6,20 +6,5 @@
 import 'package:polymer/builder.dart';
 
 main(args) {
-  build(entryPoints: [
-      'web/index.html',
-      'web/components/accordion/index.html',
-      'web/components/button/index.html',
-      'web/components/expander/index.html',
-      'web/components/flex_panel/index.html',
-      'web/components/grid_panel/index.html',
-      'web/components/progress_bar/index.html',
-      'web/components/scroll_viewer/index.html',
-      'web/components/tab/index.html',
-      'web/components/tree_view/index.html',
-      'web/getting_started/index.html',
-      'example/graph/index.html',
-      'example/list_view/index.html',
-      'example/tree_view/index.html'
-  ], options: parseOptions(args));
+  build(entryPoints: [], options: parseOptions(args));
 }
