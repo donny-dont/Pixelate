c5d2da7dd1ef9c63dcab6cef45d83125a2c98ec7
Adam Singer <financeCoding@gmail.com>
2014-04-15 23:31:44 -0700

    stage the web folder

--- drone_io.sh
+++ drone_io.sh
@@ -1,6 +1,10 @@
+#!/usr/bin/env bash
+set -o xtrace
 
 git clone https://github.com/donny-dont/Pixelate-Flat.git ../pixelate-flat
-pub install
+pub get
+
+# TODO: dartanalyzer on all libraries and main entry points in Pixelate 
 
 docgen --compile --package-root=./packages --no-include-sdk --include-private lib/*.dart
 
@@ -15,6 +19,12 @@ rm dartdoc-viewer/client/out/web/static/css/packages
 mv dartdoc-viewer/client/out/packages dartdoc-viewer/client/out/web/packages
 mv dartdoc-viewer/client/out/web ./.docs_staging
 
+# TODO: run pub build web.. This will transform Pixelate and proper packages folder
+pub build web 
+
+# TODO: stage web
+cp -r web .web_staging
+
 # fetch origin
 git fetch origin
 
@@ -25,16 +35,21 @@ git branch -v -a
 rm -rf *
 
 # copy docs up to github gh-pages branch
-#git checkout gh-pages
 git checkout --track -b gh-pages origin/gh-pages
 
 # delete any files that might still be around.
 rm -rf *
 date > date.txt
-cd .docs_staging
-cp -r . ..
-cd ../
+# TODO: unstage .web_staging
+mv .web_staging web
+# TODO: At this point Pixelate might want to just `mv .docs_staging web/docs`
+mv .docs_staging web/docs
+# 
+#cd .docs_staging
+#cp -r . ..
+#cd ../
 rm -rf .docs_staging
+rm -rf .web_staging
 git add -A
 git commit -m"auto commit from drone"
 git remote set-url origin git@github.com:donny-dont/Pixelate.git
